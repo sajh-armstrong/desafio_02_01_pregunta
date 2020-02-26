d9720ae7a34276f104ce852958d2421f17b75a6a
Sajh Armstrong <sajharmstrong@gmail.com>
2020-02-26 11:01:27 -0300

    first commit

---
+++ app/src/main/java/com/desafiolatam/desafio_02_01_pregunta_dinamica/MainActivity.java
@@ -0,0 +1,14 @@
+package com.desafiolatam.desafio_02_01_pregunta_dinamica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
